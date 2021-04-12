75654836e72cbfbdea95da5e0c38103ad2ef4abb
weirdo <1223697311@qq.com>
2021-04-12 21:30:56 +0800

    updated：个人页面中领养列表

--- reception/static/js/ajaxForJs/personal/personalAdDe.js
+++ reception/static/js/ajaxForJs/personal/personalAdDe.js
@@ -1,57 +1,92 @@
-$(function(){
-    $('.base-adopt img').click(function(e){
-        if(!e.isPropagationStopped()){
-            $('.adopt-details, .mask-box').show();
-            $('body,html').css('overflow-y', 'hidden');
+// 个人主页中领养的详情
+
+document.write("<script src='../../../../../common/js/configuration/myConfig.js'></script>")
+document.write("<script src='../../../../../common/js/cookieUtil.js'></script>")
+document.write("<script src='static/js/ajaxForJs/common/formateDate.js'></script>")
+document.write("<script src='static/js/myjs/slideshow.js'></script>")
+
+
     
-            var piId = $(this).parent("div").attr('id');
-            setCookie("piId",piId);
-            var createTime = formatDate(JSON.parse(res.data[piId].pi_createtime));
+    function adoptDeClick(){
+        $('.pet-photo').click(function(){
+            $(".adopt-details").show();
+            $('body,html').css('overflow', 'hidden');
 
-            var str = 
-            `
-            <div id="adoptDetail">
-                <div class="adopt-base">
-                    <div class="adopt-base-img">
-                        <img src="static/img/gCd0pDzhYe.jpg" alt="">
-                    </div>
-                    <div class="adopt-base-name">
-                        <h3>Beam</h3>
-                        <p>发布于${createTime}</p>
-                    </div>
-                </div>
-                <div class="pet-introduce">
-                    <h3>${res.data[piId].pi_name}</h3>
-                    <p style="padding-bottom: 8px; margin-bottom: 0px;">${res.data[piId].pi_breed}</p>
-                    <p style="padding-bottom: 8px; margin-bottom: 5px;">${res.data[piId].pi_age}</p>
-                    <div id="wrap" >
-                        <ul class="list">
-                            <li class="item active" style="width: 783px;height: 560px;"><img src="${res.data[piId].pi_image_1}" alt="" style="object-fit: cover;object-position: center;"></li>
-                            <li class="item" style="width: 783px;height: 560px;"><img src="${res.data[piId].pi_image_2}" alt="" style="object-fit: cover;object-position: center;"></li>
-                            <li class="item" style="width: 783px;height: 560px;"><img src="${res.data[piId].pi_image_3}" alt="" style="object-fit: cover;object-position: center;"></li>
-                            <li class="item" style="width: 783px;height: 560px;"><img src="${res.data[piId].pi_image_4}" alt="" style="object-fit: cover;object-position: center;"></li>
-                        </ul>
-                        <ul class="pointList">
-                            <li class="point active" data-index = '0'></li>
-                            <li class="point" data-index = '1'></li>
-                            <li class="point" data-index = '2'></li>
-                            <li class="point" data-index = '3'></li>
-                        </ul>
-                        <div class="btn fa fa-angle-left" id="goPre"></div>
-                        <div class="btn fa fa-angle-right" id="goNext"></div>
-                    </div>
-                    <p style="margin-top: 20px;">疾病史<span>：</span>${res.data[piId].pi_DH}</p>
-                    <p><span>简&nbsp;&nbsp;&nbsp;介：</span>${res.data[piId].pi_intro}</p>
-                </div>
-            </div>
+            var i = $(this).attr("id");
+            aaa(i);
+        })
+    }
 
-        `
-            $(".pet-box").prepend(str);
-            carousel();
-        }
-        setCookie("base_id",res.data[i].base_id);
-        setCookie("pet_id",res.data[i].pi_id);
 
-        e.stopPropagation();
-    })
-})
+    function aaa(i){
+        $("#adoptDe").remove();
+
+        var data = JSON.parse(getCookie("user_data"));
+        var user_id = data.user_id;
+        var myJson = JSON.stringify({"user_id": user_id})
+        $.ajax({
+            url: MyPathConfig("queryAdoptPetById"),
+            type:"post",
+            contentType:"application/json;charset=utf-8", 
+            data : myJson,
+            success(res) {
+                if (res.code == 200) {
+                    var data = res.data;
+                    var createTime = formatDate(JSON.parse(data[i].pi_createtime));
+                    var obj = ""
+                    obj +=
+                    `
+                    <div id="adoptDe">
+                        <div class="cat-decorate">
+                            <img src="static/img/new/decorate2.png" alt="">
+                        </div>
+                        <div class="adopt-base">
+                            <div class="adopt-base-img">
+                                <img src="static/img/gCd0pDzhYe.jpg" alt="">
+                            </div>
+                            <div class="adopt-base-name">
+                                <h3>Beam</h3>
+                                <p>发布于${createTime}</p>
+                            </div>
+                        </div>
+                        <div class="pet-introduce">
+                            <h3>${data[i].pi_name}</h3>
+                            <p style="padding-bottom: 8px; margin-bottom: 0px;">${data[i].pi_breed}</p>
+                            <p style="padding-bottom: 8px; margin-bottom: 5px;">${data[i].pi_age}</p>
+                            <div id="wrap" >
+                                <ul class="list" >
+                                    <li class="item active"><img src="${data[i].pi_image_1}" alt=""></li>
+                                    <li class="item"><img src="${data[i].pi_image_2}" alt=""></li>
+                                    <li class="item"><img src="${data[i].pi_image_3}" alt=""></li>
+                                    <li class="item"><img src="${data[i].pi_image_4}" alt=""></li>
+                                </ul>
+                                <ul class="pointList">
+                                    <li class="point active" data-index = '0'></li>
+                                    <li class="point" data-index = '1'></li>
+                                    <li class="point" data-index = '2'></li>
+                                    <li class="point" data-index = '3'></li>
+                                </ul>
+                                <div class="btn fa fa-angle-left" id="goPre"></div>
+                                <div class="btn fa fa-angle-right" id="goNext"></div>
+                            </div>
+                            <p style="margin-top: 20px;">疾病史<span>：</span>${data[i].pi_DH}</p>
+                            <p><span>简&nbsp;&nbsp;&nbsp;介：</span>${data[i].pi_intro}</p>
+                        </div>
+                    </div>
+                    `
+                    $(".adopt-details-top").append(obj);
+
+                    setCookie("base_id",data[i].base_id);
+                    setCookie("pet_id",data[i].pi_id);
+                    carousel();
+                }
+            },
+            error(){
+                window.location.href = "../../../common/html/rec/recError500.html";
+            }
+        })
+
+    }
+
+
+
--- reception/static/js/ajaxForJs/personal/personalAdopt.js
+++ reception/static/js/ajaxForJs/personal/personalAdopt.js
@@ -1,8 +1,6 @@
 // 个人主页中的领养列表
 document.write("<script src='../../../../../common/js/configuration/myConfig.js'></script>")
 
-
-
 $(function(){
     var data = JSON.parse(getCookie("user_data"));
     var user_id = data.user_id;
@@ -14,14 +12,13 @@ $(function(){
         data : myJson,
         success(res) {
             if(res.code == 200){
-                console.log(res.data);
                 for(var i = 0; i < res.data.length; i++){
                     var str = 
                         `
                         <div class="adoption-Pet">
-                            <div class="pet-photo">
-                                <a href="javascript:;" class="adoption-a" id="${[i]}">
-                                    <img src="${res.data[i].pi_image_1}" alt="">
+                            <div class="pet-photo"  id="${[i]}">
+                                <a href="javascript:;" class="adoption-a">
+                                    <img src="${res.data[i].pi_image_1}" alt="" >
                                 </a>
                             </div>
                             <div id="pet-content">
@@ -34,74 +31,19 @@ $(function(){
                         </div>
                         `
                         $("#personal-adopt").append(str);
-                        console.log(111);
-
-                        var data = res.data;
-                        $(".adoption-a").click(function(e){
-                            if(!e.isPropagationStopped()){
-                                var i = $(this).attr("id");
-                                console.log(data);
-                                var createTime = res.data.pi_createtime;
-                                var obj =
-                                `
-                                <div id="adoptDetail">
-                                    <div class="adopt-base">
-                                        <div class="adopt-base-img">
-                                            <img src="static/img/gCd0pDzhYe.jpg" alt="">
-                                        </div>
-                                        <div class="adopt-base-name">
-                                            <h3>Beam</h3>
-                                            <p>发布于${createTime}</p>
-                                        </div>
-                                    </div>
-                                    <div class="pet-introduce">
-                                        <h3>${res.data[i].pi_name}</h3>
-                                        <p style="padding-bottom: 8px; margin-bottom: 0px;">${res.data[i].pi_breed}</p>
-                                        <p style="padding-bottom: 8px; margin-bottom: 5px;">${res.data[i].pi_age}</p>
-                                        <div id="wrap" >
-                                            <ul class="list">
-                                                <li class="item active" style="width: 783px;height: 560px;"><img src="${res.data[i].pi_image_1}" alt="" style="object-fit: cover;object-position: center;"></li>
-                                                <li class="item" style="width: 783px;height: 560px;"><img src="${res.data[i].pi_image_2}" alt="" style="object-fit: cover;object-position: center;"></li>
-                                                <li class="item" style="width: 783px;height: 560px;"><img src="${res.data[i].pi_image_3}" alt="" style="object-fit: cover;object-position: center;"></li>
-                                                <li class="item" style="width: 783px;height: 560px;"><img src="${res.data[i].pi_image_4}" alt="" style="object-fit: cover;object-position: center;"></li>
-                                            </ul>
-                                            <ul class="pointList">
-                                                <li class="point active" data-index = '0'></li>
-                                                <li class="point" data-index = '1'></li>
-                                                <li class="point" data-index = '2'></li>
-                                                <li class="point" data-index = '3'></li>
-                                            </ul>
-                                            <div class="btn fa fa-angle-left" id="goPre"></div>
-                                            <div class="btn fa fa-angle-right" id="goNext"></div>
-                                        </div>
-                                        <p style="margin-top: 20px;">疾病史<span>：</span>${res.data[i].pi_DH}</p>
-                                        <p><span>简&nbsp;&nbsp;&nbsp;介：</span>${res.data[i].pi_intro}</p>
-                                    </div>
-                                </div>
-
-                                `
-                                setCookie("base_id",data[i].base_id);
-                                setCookie("pet_id",data[i].pi_id);
-
-                                $(".pet-box").append(obj);
-
-                                $('.pet-box, .mask-box').show();
-                                $('.pet-content').fadeIn(500);
-                                $('body,html').css('overflow-y', 'hidden');
-                                }
-                            e.stopPropagation();
-                        })
-                               
                         
-                    }
+                }
+                adoptDeClick();
+                carousel();
+
             }else if(res.code == 412){
                 $("#personal-adopt h5").html(res.message);
-                
             }
                
         },
-        error(error) {
-            console.log(error);
+        error() {
+            // window.location.href = "../../../common/html/rec/recError500.html"
+
         }
     })
 })
