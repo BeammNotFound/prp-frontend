30f2f459ae75cf3a8782c89578226512bbc544d3
weirdo <1223697311@qq.com>
2021-04-13 21:08:01 +0800

    changed：更改基地页面

--- reception/static/js/ajaxForJs/bases/bases.js
+++ reception/static/js/ajaxForJs/bases/bases.js
@@ -11,25 +11,26 @@ $(function (){
         dataType:"json",  //注意，这里是json格式
         success(res) {
             if(res.code == 200){
-            
                var str ="";
                for(var i = 0; i < res.data.length; i++){
                     if(i % 2 != 0){
                         str += 
                         `
-                        <div class="row choose-main mb-lg-4 oddNumber">
-                            <div class="col-lg-6 galsses-grid-right">
-                                
+                        <div class="row choose-main my-lg-4 my-3 oddNumber">
+                            <div class="col-lg-6 galsses-grid-right mt-lg-4">
+                                <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-lg-6 galsses-grid-right mt-4">
+
                                 <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
                                 <div class="line" data-blast="bgColor"></div>
                                 <p class="mt-3 base-main">${res.data[i].b_intro} </p>
                                 <div class="log-in mt-md-4 mt-3">
-                                    <a class="btn text-uppercase" data-blast="bgColor" target="_blank" id="${res.data[i].base_id}">
-                                    了解更多</a>
+                                    <a href="base-particulars.html" class="btn text-uppercase" data-blast="bgColor" href="javascript:;" id="${res.data[i].base_id}">
+                                        了解更多
+                                    </a>
                                 </div>
-                            </div>
-                            <div class="col-lg-6 galsses-grid-right">
-                                <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
+
                             </div>
                         </div>
 
@@ -37,21 +38,20 @@ $(function (){
                     }else{
                         str += 
                         `
-                        <div class="row choose-main my-lg-4 my-3 evenNumber">
-                            <div class="col-lg-6 galsses-grid-right mt-lg-4">
+                        <div class="row choose-main mb-lg-4 evenNumber">
+                            <div class="col-lg-6 col-lg-push-6 col-md-12 col-md-push-6 galsses-grid-right">
                                 <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
                             </div>
-                            <div class="col-lg-6 galsses-grid-right mt-4">
-        
+                            <div class="col-lg-6 col-lg-pull-6 col-md-12 col-md-pull-6 galsses-grid-right basee-introduce">
                                 <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
                                 <div class="line" data-blast="bgColor"></div>
                                 <p class="mt-3 base-main">${res.data[i].b_intro}</p>
                                 <div class="log-in mt-md-4 mt-3">
-                                    <a class="btn text-uppercase" data-blast="bgColor" target="_blank" id="${res.data[i].base_id}">
-                                    了解更多</a>
+                                    <a href="base-particulars.html" class="btn text-uppercase" data-blast="bgColor" href="javascript:;" id="${res.data[i].base_id}">
+                                        了解更多</a>
                                 </div>
-        
                             </div>
+
                         </div>
                         `
                     }
--- reception/static/js/ajaxForJs/bases/searchBases.js
+++ reception/static/js/ajaxForJs/bases/searchBases.js
@@ -38,29 +38,10 @@ function search (){
             var str ="";
             for(var i = 0; i < res.data.length; i++){
                 if(i % 2 != 0){
+                    console.log(i);
                     str += 
                     `
-                    <div class="row choose-main mb-lg-4 oddNumber">
-                        <div class="col-lg-6 galsses-grid-right">
-                            
-                            <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
-                            <div class="line" data-blast="bgColor"></div>
-                            <p class="mt-3 base-main">${res.data[i].b_intro} </p>
-                            <div class="log-in mt-md-4 mt-3">
-                                <a class="btn text-uppercase" data-blast="bgColor" target="_blank" id="${res.data[i].base_id}">
-                                了解更多</a>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 galsses-grid-right">
-                            <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
-                        </div>
-                    </div>
-
-                    `
-                }else{
-                    str += 
-                    `
-                    <div class="row choose-main my-lg-4 my-3 evenNumber">
+                    <div class="row choose-main my-lg-4 my-3 oddNumber">
                         <div class="col-lg-6 galsses-grid-right mt-lg-4">
                             <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
                         </div>
@@ -68,16 +49,39 @@ function search (){
 
                             <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
                             <div class="line" data-blast="bgColor"></div>
-                            <p class="mt-3 base-main">${res.data[i].b_intro}</p>
+                            <p class="mt-3 base-main">${res.data[i].b_intro} </p>
                             <div class="log-in mt-md-4 mt-3">
-                                <a class="btn text-uppercase" data-blast="bgColor" target="_blank" id="${res.data[i].base_id}">
-                                了解更多</a>
+                                <a href="base-particulars.html" class="btn text-uppercase" data-blast="bgColor" href="javascript:;" id="${res.data[i].base_id}">
+                                    了解更多
+                                </a>
                             </div>
 
                         </div>
                     </div>
+
+                    `
+                }else{
+                    console.log(i);
+                    str += 
+                    `
+                    <div class="row choose-main mb-lg-4 evenNumber">
+                        <div class="col-lg-6 col-lg-push-6 col-md-12 col-md-push-6 galsses-grid-right">
+                            <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-6 col-lg-pull-6 col-md-12 col-md-pull-6 galsses-grid-right basee-introduce">
+                            <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
+                            <div class="line" data-blast="bgColor"></div>
+                            <p class="mt-3 base-main">${res.data[i].b_intro}</p>
+                            <div class="log-in mt-md-4 mt-3">
+                                <a href="base-particulars.html" class="btn text-uppercase" data-blast="bgColor" href="javascript:;" id="${res.data[i].base_id}">
+                                    了解更多</a>
+                            </div>
+                        </div>
+
+                    </div>
                     `
                 }
+
             }
             $("#bases").append(str);
 
