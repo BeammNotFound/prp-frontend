5bcd0ef1915890e23adb6b4964c490f84784b539
weirdo <1223697311@qq.com>
2021-04-13 17:32:27 +0800

    changed：基地页面的搜索

--- reception/static/js/ajaxForJs/bases/searchBases.js
+++ reception/static/js/ajaxForJs/bases/searchBases.js
@@ -25,68 +25,63 @@ $(function (){
 function search (){
     $(".oddNumber").remove();
     $(".evenNumber").remove();
- var text = $("#base-search-input").val();
- var data = {"b_name" : text};
- myJson = JSON.stringify(data);
-
- $.ajax({
-    url: MyPathConfig("fuzzyQueryBases"),
-    type:"post",
-    contentType:"application/json;charset=utf-8",  //注意，这里是json格式
-    data:myJson,
-    success(res) {
-       if(res.code == 200){
-          console.log(res);
-          var str ="";
-          for(var i = 0; i < res.data.length; i++){
-            if(i%2!=0){
-                str += 
-                `
-                <div class="row choose-main mb-lg-4 oddNumber">
-                    <div class="col-lg-6 galsses-grid-right">
-                        
-                        <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
-                        <div class="line" data-blast="bgColor"></div>
-                        <p class="mt-3 base-main">${res.data[i].b_intro} </p>
-                        <div class="log-in mt-md-4 mt-3">
-                            <a class="btn text-uppercase" data-blast="bgColor" href="base-particulars.html">
-                            了解更多
-                            </a>
+    var text = $("#base-search-input").val();
+    var data = {"b_name" : text};
+    myJson = JSON.stringify(data);
+    $.ajax({
+        url: MyPathConfig("fuzzyQueryBases"),
+        type:"post",
+        contentType:"application/json;charset=utf-8",  //注意，这里是json格式
+        data:myJson,
+        success(res) {
+        if(res.code == 200){
+            var str ="";
+            for(var i = 0; i < res.data.length; i++){
+                if(i % 2 != 0){
+                    str += 
+                    `
+                    <div class="row choose-main mb-lg-4 oddNumber">
+                        <div class="col-lg-6 galsses-grid-right">
+                            
+                            <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
+                            <div class="line" data-blast="bgColor"></div>
+                            <p class="mt-3 base-main">${res.data[i].b_intro} </p>
+                            <div class="log-in mt-md-4 mt-3">
+                                <a class="btn text-uppercase" data-blast="bgColor" target="_blank" id="${res.data[i].base_id}">
+                                了解更多</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 galsses-grid-right">
+                            <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
                         </div>
                     </div>
-                    <div class="col-lg-6 galsses-grid-right">
-                        <img src="${res.data[i].b_image}" alt="" class="img-fluid">
-                    </div>
-                </div>
 
-                `
-            }else{
-                str += 
-                `
-                <div class="row choose-main my-lg-4 my-3 evenNumber">
-                    <div class="col-lg-6 galsses-grid-right mt-lg-4">
-                        <img src="${res.data[i].b_image}" alt="" class="img-fluid">
-                    </div>
-
-                    <div class="col-lg-6 galsses-grid-right mt-4">
-                        <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
-                        <div class="line" data-blast="bgColor"></div>
-                        <p class="mt-3 base-main">${res.data[i].b_intro}</p>
-                        <div class="log-in mt-md-4 mt-3">
-                            <a class="btn text-uppercase" data-blast="bgColor" href="base-particulars.html">
-                            了解更多
-                            </a>
+                    `
+                }else{
+                    str += 
+                    `
+                    <div class="row choose-main my-lg-4 my-3 evenNumber">
+                        <div class="col-lg-6 galsses-grid-right mt-lg-4">
+                            <img src="${res.data[i].b_icon}" alt="" class="img-fluid">
                         </div>
+                        <div class="col-lg-6 galsses-grid-right mt-4">
 
+                            <h4 class="post mt-3 base-name">${res.data[i].b_name}</h4>
+                            <div class="line" data-blast="bgColor"></div>
+                            <p class="mt-3 base-main">${res.data[i].b_intro}</p>
+                            <div class="log-in mt-md-4 mt-3">
+                                <a class="btn text-uppercase" data-blast="bgColor" target="_blank" id="${res.data[i].base_id}">
+                                了解更多</a>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-                `
+                    `
+                }
             }
+            $("#bases").append(str);
 
-       }
-       $(".inner-sec-w3layouts").append(str);
-        
-       }else if(res.code == 412){
+        }else if(res.code == 412){
             var str =
             `
             <div style = "width:100%;height:200px;" class="row choose-main mb-lg-4 oddNumber">
@@ -95,10 +90,12 @@ function search (){
                 
             </div>
             `
-            $(".inner-sec-w3layouts").append(str);
-            console.log(res.message);
-       }
-    }
- })
+            $("#bases").append(str);
+        }
+        },
+        error(){
+            window.location.href = "../../../common/html/rec/recError500.html"
+        }
+    })
 
 }
