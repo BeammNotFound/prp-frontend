7bf8a0ca31547b1afd2d0e1d678665076a096a94
weirdo <1223697311@qq.com>
2021-04-21 11:12:28 +0800

    changed：在页面缩小时候的导航栏登录

--- reception/static/js/ajaxForJs/login/checkLogin.js
+++ reception/static/js/ajaxForJs/login/checkLogin.js
@@ -13,17 +13,28 @@ $(function (){
         $("#personal span").append(res.user_name);
         $("#personal-img").attr("src",res.user_icon);
         $("#personal-a").attr("href", "javascript:;");
-
+        //导航栏 
+        var n=0;
+        $('.slicknav_arrow').on('click', function(){
+            n++;
+            if(n % 2 !== 0){
+                $('.slicknav_hidden').slideDown(200);
+                $('.slicknav_arrow').html('-');
+            }
+            else{
+                $('.slicknav_hidden').slideUp(200);
+                $('.slicknav_arrow').html('+');
+            }
+        })
+        // 判断是否为管理员身份
         if(res.user_type == 2){
             $("#manage").css("display","block");
         }
         return;
 
-        
     }else{
         $("#personal-a").attr("href", "login.html");
         $(".slicknav_arrow").css("display","none");
+        $(".personal ").children("a").attr("href","login.html");
     }
-    
-
 })
--- reception/static/js/myjs/nav.js
+++ reception/static/js/myjs/nav.js
@@ -1,21 +1,20 @@
-//导航栏
-var n=0;
-$('.slicknav_arrow').on('click', function(){
-    n++;
-    console.log(1111)
-    if(n % 2 !== 0){
-        $('.slicknav_hidden').slideDown(200);
-        $('.slicknav_arrow').html('-');
-    }
-    else{
-        $('.slicknav_hidden').slideUp(200);
-        $('.slicknav_arrow').html('+');
-    }
-})
+// var n=0;
+
+// $('.slicknav_arrow').on('click', function(){
+//     n++;
+//     console.log(1111)
+//     if(n % 2 !== 0){
+//         $('.slicknav_hidden').slideDown(200);
+//         $('.slicknav_arrow').html('-');
+//     }
+//     else{
+//         $('.slicknav_hidden').slideUp(200);
+//         $('.slicknav_arrow').html('+');
+//     }
+// })
 
 var m=0;
 $('.slicknav_btn').on('click', function(){
-    console.log(2222)
     m++;
     if(m % 2 !== 0){
         $('.slicknav_nav').slideDown(200);
