3c9966eb8a0b5415a8258cd87a2be3d989a384e1
Beam <beamm0613@163.com>
2021-05-10 12:58:12 +0800

    更改服务器地址

--- reception/management/static/js/config/myConfig.js
+++ reception/management/static/js/config/myConfig.js
@@ -1,5 +1,5 @@
 function MyPathConfig(Path){
-    var DefaultPath = "http://47.94.104.224:8080/";
+    var DefaultPath = "http://101.132.121.178:8080";
     // var DefaultPath = "http://192.168.1.233:8080/";
     return DefaultPath + Path;
 }
